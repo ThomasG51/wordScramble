b917bbefeb54951a985eafb13681b36c89f4e304
Thomas <contact@thomas-george.com>
2021-05-18 19:27:33 +0200

    Initial Commit

---
+++ Project_5_WordScramble/Project_5_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_5_WordScrambleApp.swift
+//  Project_5_WordScramble
+//
+//  Created by Thomas George on 18/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Project_5_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
